750fc96420151ed8257abc35a7dd6b1ccd2fe761
wjgin <woojin0913@naver.com>
2022-05-04 00:59:16 +0900

    refac: change open resource to http url

--- auto_search.sh
+++ auto_search.sh
@@ -1,11 +1,18 @@
+#!/bin/bash
 # auto_search scrip
 
 echo this is test...
 
-path=/Applications
+#path=/Applications
+#program=Google\ Chrome.app
+#open "$path/$program
 
-program=Google\ Chrome.app
+query_name1="보드카페"
+query_name2="일산보드카페"
 
-open "$path/$program"
+query_result1="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=$query_name1&oquery=&tqi=hFaMBdprvxsssTWhZJCssssstTo-188364"
 
 
+query_result2="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=$query_name2&oquery=&tqi=hFaMBdprvxsssTWhZJCssssstTo-188364"
+
+open $query_result1 $query_result2

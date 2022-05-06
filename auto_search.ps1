d453fb54a0d4f614d54fa37eaf57637c51166d0f
wjgin <woojin0913@naver.com>
2022-05-06 23:13:09 +0900

    chore: version 1.0.0 open 3 tabs keyword1, keyword2, result map

--- auto_search.ps1
+++ auto_search.ps1
@@ -5,45 +5,45 @@
 $env:LC_ALL='C.UTF-8';
 # variable section
 $KEY_WORD1=get-content -encoding utf8 "var1.txt";    # 보드게임카페
-$KEY_WORD1=get-content -encoding utf8 "var2.txt";    # 일산보드게임카페
-#$KEY_WORD1_URL="https://search.naver.com/search.naver?query=$KEY_WORD1"
-#$KEY_WORD2_URL="https://search.naver.com/search.naver?query=$KEY_WORD2"
+$KEY_WORD2=get-content -encoding utf8 "var2.txt";    # 일산보드게임카페
 
-#$KEY_WORD2_URL="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=$KEY_WORD2&oquery=&tqi=hFaMBdprvxsssTWhZJCssssstTo-188364"
-#$KEY_WORD1_MAP="https://map.naver.com/v5/search/$KEY_WORD1/place/1733463308?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&c=14111718.1952715,4531066.7676903,15,0,0,0,dh"
-#$KEY_WORD2_MAP="https://map.naver.com/v5/search/$KEY_WORD2/place/1733463308?placePath=%3Fentry=pll%26from=nx%26fromNxList=true&c=14111718.1952715,4531066.7676903,15,0,0,0,dh"
+$KEY_WORD1_URL="https://search.naver.com/search.naver?query=$KEY_WORD1"
+$KEY_WORD2_URL="https://search.naver.com/search.naver?query=$KEY_WORD2"
 
-
-
-
+$KEY_WORD_MAP="https://map.naver.com/v5/entry/place/1733463308";
 
 $PROGRAM_NAME="iexplore"
-$WAITTING_TIME=5  #sec
-$LOOP_COUNT=1
+$WAITTING_TIME=10  #sec
+$LOOP_COUNT=2
+
 
 For ($i=1; $i -le $LOOP_COUNT; $i++) {  # loop
 
     $ie=New-Object -COM 'InternetExplorer.Application'; # create ie application object
     $ie.visible=$true;  # show visual
-    #$ie.navigate($KEY_WORD1_URL);   # open first tab
-    #$ie.navigate($KEY_WORD2_URL, 0x1000);   # create new tab   
-    
-    
-    $ie.navigate("https://www.naver.com");  
+    $ie.navigate($KEY_WORD1_URL);   # open first tab
+    $ie.navigate($KEY_WORD2_URL, 0x1000);   # create new tab   
+    $ie.navigate($KEY_WORD_MAP, 0x1000);   # create new tab   
 
     while ($ie.Busy -eq $true) {Start-Sleep -seconds 1;}
 
-    $sText=$ie.Document.getElementByID("query");
-    $sText.Value=$KEY_WORD1;
-
-    $sform=$ie.Document.getElementByID("sform");
-    sleep 3;
-    $sform.submit();
-
     sleep $WAITTING_TIME;   # sleep after opening all tabs
 
-    # get-Process iexplore | stop-Process;    # kill ie process
+    get-Process iexplore | stop-Process;    # kill ie process
 
-    sleep 1 # sleep after ie process
+    sleep 1; # sleep after ie process
 }
 
+ #   $li=$ie.Document.getElementsByTagName('li') | where {$_.getAttributeNode('data-nmb_atl-doc-id').Value -eq '1753908673'}
+ #   $a=$li.contentwindow.document.body.getElementsByTagName('a');
+ #   $a.click();
+
+# 직접 클릭 방법
+# $ie.navigate("https://www.naver.com");  
+# $sText=$ie.Document.getElementByID("query");
+# $sText.Value=$KEY_WORD1;
+
+# while ($ie.Busy -eq $true) {Start-Sleep -seconds 1;}
+
+# $sform=$ie.Document.getElementByID("sform");
+# $sform.submit();
